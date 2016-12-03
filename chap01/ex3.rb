0dc70a20bffff2e08b0ca3a3f6b4c7b73132c487
micawhitby <micawhitby@gmail.com>
2016-12-03 14:52:26 +0000

    first 5 exercises complete

---
+++ chap01/ex3.rb
@@ -0,0 +1,31 @@
+#Writes string
+puts "I will now count my chickens:"
+
+# Calculates and writes how many hens and roosters
+puts "Hens #{25.0 + 30.0 / 6.0}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+#Writes string
+puts "Now i will count the eggs"
+
+#Calculates and writes
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+#Writes and asks the question, returns a false
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#Another calculation
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+#Calculates the number in brackets and returns the answer
+puts "What is 3.0 + 2.0? #{3.0 + 2.0}"
+puts "What is 5.0 - 7.0? #{5.0 - 7.0}"
+
+#Writes string
+puts "Oh, that's why it's false."
+puts "How about some more."
+
+#Returns true or false for each question
+puts "Is it greater? #{5.0 > -2.0}"
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+puts "Is it less or equal? #{5.0 <= -2.0}"
