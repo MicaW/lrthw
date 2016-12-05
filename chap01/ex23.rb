9eaa79a244537b1220d9025740ccacc3f3946256
micawhitby <micawhitby@gmail.com>
2016-12-05 18:33:41 +0000

    Completed 25 exercises

---
+++ chap01/ex23.rb
@@ -0,0 +1,3 @@
+print "bitbucket.org,\ngithub.com,\n gitorious.org,\n github.com,\n launchpad.net,\n sourceforge.net\n can be used to look at other people's source code and try to understand it."
+
+puts "You can click on the source tab, and browse through the lists of files and directories until you find an rb file. Read through the code, and then research any symbols or words that are unrecognised."
